c299109b03d0389a89a0fe106ce83c9f6756db4d
ianquill <ian@ianquill.com>
2021-09-21 18:44:50 -0700

    added functionality to buttons, working on functions

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let currentOperator;
+let numA;
+let numB;
+
 function add(a, b) {
     return a + b;
 }
@@ -34,4 +38,122 @@ function operate(a, b, operator) {
 
 const display = document.querySelector(".display");
 
-display.textContent = 696969;
+let displayContent = 6969691;
+
+// display.textContent = "";
+
+function refreshDisplay() {
+    // add currentOperator display to this
+
+    display.textContent = displayContent;
+    return;
+}
+
+// buttons
+
+function pressButton(button) {
+    
+    switch (button) {
+        case "clear":
+            displayContent = "";
+            refreshDisplay(); 
+            break;
+
+        case "backspace":
+            // maybe convert to array, remove last digit?
+            // convert back
+            refreshDisplay(); 
+            break;
+
+        case "divide":
+            currentOperator = "/";
+            refreshDisplay(); 
+            break;
+
+        case "multiply":
+            currentOperator = "*";
+            refreshDisplay(); 
+            break;
+
+        case "subtract":
+            currentOperator = "-";
+            refreshDisplay(); 
+            break;
+
+        case "add":
+            currentOperator = "+";
+            refreshDisplay(); 
+            break;
+
+        case "decimal":
+            displayContent = "";
+            refreshDisplay(); 
+            break;
+
+        case "equals":
+            displayContent = operate(a, b, currentOperator);
+            refreshDisplay(); 
+            break;
+
+        case "zero":
+            displayContent += 0;
+            refreshDisplay(); 
+            break;
+
+        case "one":
+            displayContent += 1;
+            refreshDisplay(); 
+            break;
+            
+        case "two":
+            displayContent += 2;
+            refreshDisplay(); 
+            break;
+
+        case "three":
+            displayContent += 3;
+            refreshDisplay(); 
+            break;
+
+        case "four":
+            displayContent += 4;
+            refreshDisplay(); 
+            break;
+
+        case "five":
+            displayContent += 5;
+            refreshDisplay(); 
+            break;
+
+        case "six":
+            displayContent += 6;
+            refreshDisplay(); 
+            break;
+
+        case "seven":
+            displayContent += 7;
+            refreshDisplay(); 
+            break;
+
+        case "eight":
+            displayContent += 8;
+            refreshDisplay(); 
+            break;
+
+        case "nine":
+            displayContent += 9;
+            refreshDisplay(); 
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+const buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        pressButton(button.id);
+    })
+})
