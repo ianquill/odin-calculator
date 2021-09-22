17f51c2e0003c5fd84a716597c01647a23a0b937
ianquill <ian@ianquill.com>
2021-09-22 16:14:53 -0700

    added backspace functionality

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
 # TODO
 
-    - decimals rounded
-    - overflow fix
-    - backspace functionality
     - keyboard support
+    - add footer
 
     - low priority: fix console error for clicking decimal when equals has calculated a number with a decimal
-    - after error overflow, you can still keep calculating
 
--- script.js
+++ script.js
@@ -44,11 +44,15 @@ function operate(a, b, operator) {
 
 function roundResult(result) {
     // check length of result, if more than 15, take the difference and round to that many digits
-    if (result.length > 15) {
-        let resultDifference = result.length - 15;
-        let roundedResult = result.toPrecision(resultDifference);
+    console.log("result is " + result);
+    if (result.toString().length > 15) {
+        console.log("rounded");
+        let resultDifference = result.toString().length - 13;
+        let roundedResult = result.toPrecision((result.toString().length - resultDifference));
         console.log(roundedResult);
         return roundedResult;
+    } else {
+        return result;
     }
 
 }
@@ -64,22 +68,27 @@ let subDisplayContent;
 let operatorDisplayContent;
 let result = false;
 
+let suspend = false;
+
 
 function refreshDisplay() {
     console.log(mainDisplayContent.length);
 
-    // if (mainDisplayContent.length < 15) {
-    //     mainDisplay.textContent = mainDisplayContent;
-    //     subDisplay.textContent = subDisplayContent;
-    //     operatorDisplay.textContent = operatorDisplayContent;
-    // } else if (mainDisplayContent.length >= 15) {
-    //     mainDisplayContent = "ERROR: OVERFLOW";
-    //     mainDisplay.textContent = mainDisplayContent;
-    // }
+    if (mainDisplayContent.toString().length < 15) {
+        mainDisplay.textContent = mainDisplayContent;
+        subDisplay.textContent = subDisplayContent;
+        operatorDisplay.textContent = operatorDisplayContent;
+    } else if (mainDisplayContent.toString().length >= 15) {
+        suspend = true;
+        operatorDisplayContent = "";
+        operatorDisplay.textContent = operatorDisplayContent;
+        mainDisplayContent = "ERROR: OVERFLOW";
+        mainDisplay.textContent = mainDisplayContent;
+    }
 
-    mainDisplay.textContent = mainDisplayContent;
-    subDisplay.textContent = subDisplayContent;
-    operatorDisplay.textContent = operatorDisplayContent;
+    // mainDisplay.textContent = mainDisplayContent;
+    // subDisplay.textContent = subDisplayContent;
+    // operatorDisplay.textContent = operatorDisplayContent;
 
 }
 
@@ -114,23 +123,25 @@ function pressButton(button) {
             mainDisplayContent = "";
             operatorDisplayContent = "";
             result = false;
+            suspend = false;
             refreshDisplay(); 
             break;
 
         case "backspace":
-            // maybe convert to array, remove last digit?
-            // convert back
+            let charArray = Array.from(mainDisplayContent.toString().split(""));
+            charArray.pop();
+            mainDisplayContent = charArray.join("");
             refreshDisplay(); 
-            break;
+            break; 
 
         case "divide":
             result = false;
-            if (checkDisplaysFull()) {
+            if (checkDisplaysFull() && !suspend) {
                 subDisplayContent = operate(subDisplayContent, mainDisplayContent, operatorDisplayContent);
                 operatorDisplayContent = "/";
                 mainDisplayContent = "";
                 refreshDisplay();
-            } else if (checkMainDisplay()) {
+            } else if (checkMainDisplay() && !suspend) {
                 operatorDisplayContent = "/";
                 fillSubDisplay();
                 refreshDisplay(); 
@@ -139,12 +150,12 @@ function pressButton(button) {
 
         case "multiply":
             result = false;
-            if (checkDisplaysFull()) {
+            if (checkDisplaysFull() && !suspend) {
                 subDisplayContent = operate(subDisplayContent, mainDisplayContent, operatorDisplayContent);
                 operatorDisplayContent = "*";
                 mainDisplayContent = "";
                 refreshDisplay();
-            } else if (checkMainDisplay()) {
+            } else if (checkMainDisplay() && !suspend) {
                 operatorDisplayContent = "*";
                 fillSubDisplay();
                 refreshDisplay(); 
@@ -153,12 +164,12 @@ function pressButton(button) {
 
         case "subtract":
             result = false;
-            if (checkDisplaysFull()) {
+            if (checkDisplaysFull() && !suspend) {
                 subDisplayContent = operate(subDisplayContent, mainDisplayContent, operatorDisplayContent);
                 mainDisplayContent = "";
                 operatorDisplayContent = "-";
                 refreshDisplay();
-            } else if (checkMainDisplay()) {
+            } else if (checkMainDisplay() && !suspend) {
                 operatorDisplayContent = "-";
                 fillSubDisplay();
                 refreshDisplay(); 
@@ -167,12 +178,12 @@ function pressButton(button) {
 
         case "add":
             result = false;
-            if (checkDisplaysFull()) {
+            if (checkDisplaysFull() && !suspend) {
                 subDisplayContent = operate(subDisplayContent, mainDisplayContent, operatorDisplayContent);
                 operatorDisplayContent = "+";
                 mainDisplayContent = "";
                 refreshDisplay();
-            } else if (checkMainDisplay()) {
+            } else if (checkMainDisplay() && !suspend) {
                 operatorDisplayContent = "+";
                 fillSubDisplay();
                 refreshDisplay(); 
@@ -180,14 +191,14 @@ function pressButton(button) {
             break;
 
         case "decimal":
-            if (!mainDisplayContent.includes('.')){
+            if (!mainDisplayContent.includes('.') && !suspend){
                 mainDisplayContent += ".";
                 refreshDisplay(); 
             }
             break;
 
         case "equals":
-            if (mainDisplayContent !== "" & subDisplayContent !== "" & operatorDisplayContent !== "") {
+            if (mainDisplayContent !== "" & subDisplayContent !== "" & operatorDisplayContent !== "" && !suspend) {
                 console.log("equals");
                 mainDisplayContent = operate(subDisplayContent, mainDisplayContent, operatorDisplayContent);
                 subDisplayContent = "";
@@ -199,70 +210,70 @@ function pressButton(button) {
             break;
 
         case "zero":
-            if (!result) {
+            if (!result && !suspend) {
                 mainDisplayContent += 0;
                 refreshDisplay(); 
             }
             break;
 
         case "one":
-            if (!result) {
+            if (!result && !suspend) {
                 mainDisplayContent += 1;
                 refreshDisplay(); 
             }
             break;
             
         case "two":
-            if (!result) {
+            if (!result && !suspend) {
                 mainDisplayContent += 2;
                 refreshDisplay(); 
             }
             break;
 
         case "three":
-            if (!result) {
+            if (!result && !suspend) {
                 mainDisplayContent += 3;
                 refreshDisplay(); 
             }
             break;
 
         case "four":
-            if (!result) {
+            if (!result && !suspend) {
                 mainDisplayContent += 4;
                 refreshDisplay(); 
             }
             break;
 
         case "five":
-            if (!result) {
+            if (!result && !suspend) {
                 mainDisplayContent += 5;
                 refreshDisplay(); 
             }
             break;
 
         case "six":
-            if (!result) {
+            if (!result && !suspend) {
                 mainDisplayContent += 6;
                 refreshDisplay(); 
             }
             break;
 
         case "seven":
-            if (!result) {
+            if (!result && !suspend) {
                 mainDisplayContent += 7;
                 refreshDisplay(); 
             }
             break;
 
         case "eight":
-            if (!result) {
+            if (!result && !suspend) {
                 mainDisplayContent += 8;
                 refreshDisplay(); 
             }
             break;
 
         case "nine":
-            if (!result) {
+            if (!result && !suspend) {
                 mainDisplayContent += 9;
                 refreshDisplay(); 
             }
